7853f384c72cdbbaed9ae8daa568c2f7b58b9125
codewithdeepak-dk <dipakhjp6972@gmail.com>
2026-02-15 23:52:52 +0530

    Mobile.java

---
+++ E-Commerce-Project/Mobile.java
@@ -0,0 +1,39 @@
+package ECommerceProject;
+
+public class Mobile extends Product{
+	static private int count=0;
+	{
+		count++;
+	}
+	public static int getCount() {
+		return count;
+	}
+	private final int ram;
+	private final String color;
+	private final int hd;
+	
+	Mobile(String brand, double price, int ram, String color,int hd){
+		super(brand, price);
+		this.ram=ram;
+		this.color=color;
+		this.hd=hd;
+	}
+	public int getRam() {
+		return ram;
+	}
+	public String getColor() {
+		return color;
+	}
+	public int getHd() {
+		return hd;
+	}
+	@Override
+	public void buy() {
+		//code to process the purchase of the mobile
+		System.out.println("Mobile bought: " + getBrand() + "\tRAM: " + ram + "GB\tColor: " + color + "\tHD: " + hd + "GB\tPrice: " + getPrice());
+	}
+	@Override
+	public String toString() {
+		return super.toString()+"\tRAM: " + ram + "GB\tColor: " + color + "\tHD: " + hd + "GB";
+	}
+}
